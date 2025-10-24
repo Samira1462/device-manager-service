54d71975faf97ca8c1d79df709ae654b998104bd
samira <samira.radmaneshfar@gmail.com>
2025-10-24 13:41:40 +0200

    create maven project

---
+++ src/test/java/com/codechallenge/device_manager_service/DeviceManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.device_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
